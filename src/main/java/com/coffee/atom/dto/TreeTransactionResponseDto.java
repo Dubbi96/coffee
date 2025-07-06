9ae02e8f652d7ab0e1c85ffc09ac5003d4db6cc7
Dubbi96 <yrs03001@hanyang.ac.kr>
2025-07-07 08:25:14 +0900

    fix : 나무 수령 목록 조회에서 면장, 지역, 섹션 조회 추가

--- src/main/java/com/coffee/atom/dto/TreeTransactionResponseDto.java
+++ src/main/java/com/coffee/atom/dto/TreeTransactionResponseDto.java
@@ -12,6 +12,12 @@ public class TreeTransactionResponseDto {
     private Long id;
     private Long farmerId;
     private String farmerName;
+    private Long villageHeadId;
+    private String villageHeadName;
+    private Long areaId;
+    private String areaName;
+    private Long sectionId;
+    private String sectionName;
     private String species;
     private Long quantity;
     private LocalDate receivedDate;
@@ -21,6 +27,12 @@ public class TreeTransactionResponseDto {
                 .id(tx.getId())
                 .farmerId(tx.getFarmer().getId())
                 .farmerName(tx.getFarmer().getName())
+                .villageHeadId(tx.getFarmer().getVillageHead().getId())
+                .villageHeadName(tx.getFarmer().getVillageHead().getAppUser().getUsername())
+                .areaId(tx.getFarmer().getVillageHead().getSection().getArea().getId())
+                .areaName(tx.getFarmer().getVillageHead().getSection().getArea().getAreaName())
+                .sectionId(tx.getFarmer().getVillageHead().getSection().getId())
+                .sectionName(tx.getFarmer().getVillageHead().getSection().getSectionName())
                 .species(tx.getSpecies())
                 .quantity(tx.getQuantity())
                 .receivedDate(tx.getReceivedDate())
